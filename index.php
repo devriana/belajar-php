42dbb8926ebdbfdd56c52877be1a782fec4f3914
Devi Riana Meihesty <devirianameyhesti@gmail.com>
2022-10-29 14:10:37 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World!";
+
+$nama = "Devi Riana";
+$umur = 20;
+
+echo "Nama saya $nama, saya berusia $umur tahun.<br>";
+
+
+$namaAdik = "Lisa";
+$umurAdik = 17;
+
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia$umurAdik tahun.<br>";
+
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+?>
